8cd9e0f2ca4eac713167ed27c21201eae3f0a74c
Karsten Schmidt <git@guggemand.dk>
2011-12-27 10:52:01 +0100

    Simple snapshot script.

---
+++ snap.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+FS=$1
+ZFS=$(which zfs)
+DATE=$(which date)
+
+if [ -z "$1" ]; then
+  echo Usage: $0 FileSystem
+  exit 1
+fi
+
+if [ ! -x "$ZFS" ]; then
+  echo "zfs binary is missing!"
+  exit 1
+fi
+
+if [ ! -x "$DATE" ]; then
+  echo "date binary is missing!"
+  exit 1
+fi
+
+TIME=$(date +"%Y%m%d-%H%M%S")
+
+if ! $ZFS list -H $FS > /dev/null 2>&1; then
+  echo Invalid FileSystem
+  exit 1
+fi
+
+$ZFS snapshot $FS@snap-$TIME
+
