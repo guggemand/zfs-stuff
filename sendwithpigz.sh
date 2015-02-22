e088f6f13d05e96cdf4493b58460da850789bc4e
Karsten Schmidt <git@guggemand.dk>
2015-02-22 20:32:31 +0100

    Add helper script to sync snapshots with pigz and mbuffer.

---
+++ sendwithpigz.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+# Helper script for sync.sh
+# It will send the stream compressed over ssh to remotehost.
+# If mbuffer is installed it will use a 1G buffer to speed up the transfer.
+#
+# set dlx.dk.sync:remotecmd to "sendwithpigz.sh user@remotehost"
+
+if [ -z "$1" -o -z "$2" ]; then
+  echo Usage: $0 user@host list/receive
+  exit 1
+fi
+
+REMOTEHOST=$1
+MBUFFER=$(which mbuffer)
+
+shift
+
+if [ "$1" = "receive" ]; then
+  if [ -x "$MBUFFER" ]; then
+    $MBUFFER -m 1G -q | pigz | ssh -c arcfour $REMOTEHOST pigz -d \| /sbin/zfs $*
+  else
+    pigz | ssh -c arcfour $REMOTEHOST pigz -d \| /sbin/zfs $*
+  fi
+elif [ "$1" = "list" ]; then
+  ssh $REMOTEHOST /sbin/zfs $*
+else
+  echo "only \"list\" and \"receive\" is supported"
+  exit 1
+fi
+
