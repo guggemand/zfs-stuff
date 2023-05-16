b3d27598f9f2228fb2228935566d3939811a7e17
Karsten Schmidt <git@guggemand.dk>
2023-05-16 09:56:17 +0200

    Include volumes in syncall

--- syncall.sh
+++ syncall.sh
@@ -9,7 +9,7 @@ fi
 
 DIR=$(dirname $0)
 
-for fs in $($ZFS get -s local -t filesystem -o name -H dlx.dk.sync:remotefs); do
+for fs in $($ZFS get -s local -t filesystem,volume -o name -H dlx.dk.sync:remotefs); do
   $DIR/sync.sh $fs
 done
 
