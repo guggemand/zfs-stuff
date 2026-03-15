56052b08f9c10038220a6b6342f79356124ad81b
Karsten Schmidt <ks@dlx.dk>
2026-03-15 21:29:05 +0100

    Clean up authorized_keys_commands.sh and disable globbing

--- authorized_keys_commands.sh
+++ authorized_keys_commands.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-# 
+
+set -e
+
+#
 # commands script for openssh authorized_keys
 #
-# Use this in ~/.ssh/.ssh/authorized_keys
+# Use this in ~/.ssh/authorized_keys
 # command="/path/to/authorized_keys_commands.sh",no-port-forwarding,no-X11-forwarding,no-pty ssh-rsa .........
 
+set -f
 set -- $SSH_ORIGINAL_COMMAND
 
 if [ "$1 $2 $3" = "pigz -d |" ]; then
@@ -12,11 +16,11 @@ if [ "$1 $2 $3" = "pigz -d |" ]; then
   shift 3
 fi
 
-if [ "$1" = "/sbin/zfs" -o "$1" = "zfs" ]; then
+if [ "$1" = "/sbin/zfs" ] || [ "$1" = "zfs" ]; then
   case "$2" in
     "list")
         if [ "$3 $4 $5 $6 $7 $8 $9" = "-t snapshot -s creation -o name -rH" ]; then
-          $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10}
+          "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9" "${10}"
           exit $?
         fi
       ;;
@@ -24,16 +28,16 @@ if [ "$1" = "/sbin/zfs" -o "$1" = "zfs" ]; then
     "receive")
         if [ "$3" = "-F" ]; then
           if [ -n "$PIGZ" ]; then
-            pigz -d | $1 $2 $3 $4
+            pigz -d | "$1" "$2" "$3" "$4"
           else
-            $1 $2 $3 $4
+            "$1" "$2" "$3" "$4"
           fi
           exit $?
         else
           if [ -n "$PIGZ" ]; then
-            pigz -d | $1 $2 $3
+            pigz -d | "$1" "$2" "$3"
           else
-            $1 $2 $3
+            "$1" "$2" "$3"
           fi
           exit $?
         fi
@@ -41,6 +45,6 @@ if [ "$1" = "/sbin/zfs" -o "$1" = "zfs" ]; then
   esac
 fi
 
-echo "'$SSH_ORIGINAL_COMMAND' not allowed"
+echo "'$SSH_ORIGINAL_COMMAND' not allowed" >&2
 exit 1
 
