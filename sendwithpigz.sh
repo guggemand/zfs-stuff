d406a5f156450f9d20a916d3c9ba7e44e01499c0
Karsten Schmidt <ks@dlx.dk>
2026-03-15 21:29:22 +0100

    Clean up sendwithpigz.sh and suppress mbuffer warnings

--- sendwithpigz.sh
+++ sendwithpigz.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+set -e
+
 #
 # Helper script for sync.sh
 # It will send the stream compressed over ssh to remotehost.
@@ -6,27 +9,27 @@
 #
 # set dlx.dk.sync:remotecmd to "sendwithpigz.sh user@remotehost"
 
-if [ -z "$1" -o -z "$2" ]; then
-  echo Usage: $0 user@host list/receive
+if [ -z "$1" ] || [ -z "$2" ]; then
+  echo "Usage: $0 user@host list/receive" >&2
   exit 1
 fi
 
 REMOTEHOST=$1
-MBUFFER=$(which mbuffer)
+MBUFFER=$(command -v mbuffer)
 PATH=$PATH:/usr/local/bin:/usr/local/sbin
 
 shift
 
 if [ "$1" = "receive" ]; then
   if [ -x "$MBUFFER" ]; then
-    $MBUFFER -m 1G -q -s 128k | pigz | ssh $REMOTEHOST pigz -d \| /sbin/zfs $*
+    $MBUFFER -m 1G -q -s 128k 2>/dev/null | pigz | ssh "$REMOTEHOST" pigz -d \| /sbin/zfs "$@"
   else
-    pigz | ssh $REMOTEHOST pigz -d \| /sbin/zfs $*
+    pigz | ssh "$REMOTEHOST" pigz -d \| /sbin/zfs "$@"
   fi
 elif [ "$1" = "list" ]; then
-  ssh $REMOTEHOST /sbin/zfs $*
+  ssh "$REMOTEHOST" /sbin/zfs "$@"
 else
-  echo "only \"list\" and \"receive\" is supported"
+  echo "only \"list\" and \"receive\" is supported" >&2
   exit 1
 fi
 
